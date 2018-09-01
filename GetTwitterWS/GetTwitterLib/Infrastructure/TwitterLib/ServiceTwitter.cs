3653743f3e1518469184f6baf71efd909822920e
osnjunior91 <junin096>
2018-09-01 18:41:50 -0300

    Ajuste dependencias do projeto

--- GetTwitterWS/GetTwitterLib/Infrastructure/TwitterLib/ServiceTwitter.cs
+++ GetTwitterWS/GetTwitterLib/Infrastructure/TwitterLib/ServiceTwitter.cs
@@ -1,23 +1,21 @@
 ﻿using GetTwitterLib.Model;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
-using TweetSharp;
 
 namespace GetTwitterLib.Infrastructure.Twitter
 {
     public class ServiceTwitter : IDisposable
     {
-        private TwitterService Service {
-            get
-            {
-                return new TwitterService(
-                    ConfigurationManager.AppSettings["CustomerKey"],
-                    ConfigurationManager.AppSettings["CustomerKeySecret"],
-                    ConfigurationManager.AppSettings["AcessToken"],
-                    ConfigurationManager.AppSettings["AcessTokenSecret"]);
-            }
-        }
+        //private TwitterService Service {
+        //    get
+        //    {
+        //        return new TwitterService(
+        //            ConfigurationManager.AppSettings["CustomerKey"],
+        //            ConfigurationManager.AppSettings["CustomerKeySecret"],
+        //            ConfigurationManager.AppSettings["AcessToken"],
+        //            ConfigurationManager.AppSettings["AcessTokenSecret"]);
+        //    }
+        //}
         public void Dispose()
         {
         }
@@ -25,7 +23,7 @@ namespace GetTwitterLib.Infrastructure.Twitter
         public List<TwitterObject> GetTwitter()
         {
 
-            var twitterResult = Service.Search(new SearchOptions { Q  = "#HaddadPresidente", Resulttype = TwitterSearchResultType.Recent, SinceId = 29999 });
+            //var twitterResult = Service.Search(new SearchOptions { Q  = "#HaddadPresidente", Resulttype = TwitterSearchResultType.Recent, SinceId = 29999 });
 
             return null;
         }
