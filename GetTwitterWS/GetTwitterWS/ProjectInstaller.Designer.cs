ec092df6a94cb5fc1cc3166c641037ff08672128
osnjunior91 <junin096>
2018-09-02 18:26:14 -0300

    Ajuste serviço

---
+++ GetTwitterWS/GetTwitterWS/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace GetTwitterWS
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.GetTwitterData = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // GetTwitterData
+            // 
+            this.GetTwitterData.DisplayName = "TCC - GetTwitterData";
+            this.GetTwitterData.ServiceName = "TCC - GetTwitterData";
+            this.GetTwitterData.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.GetTwitterData});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller GetTwitterData;
+    }
+}
--- GetTwitterWS/GetTwitterWS/Service1.cs
+++ GetTwitterWS/GetTwitterWS/Service1.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using GetTwitterLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GetTwitterWS
 {
     public partial class Service1 : ServiceBase
     {
+        private string arquivoErro = @"c:\LogTcc\erroTcc.txt";
+        private string arquivoConexaoFechada = @"c:\LogTcc\conexaoFechada.txt";
+        private string arquivoSuceso = @"c:\LogTcc\sucessoTcc.txt";
+        Timer timer1;
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +26,58 @@ namespace GetTwitterWS
 
         protected override void OnStart(string[] args)
         {
+            timer1 = new Timer(new TimerCallback(GetTwitter), null, 0, 300000);
         }
 
         protected override void OnStop()
         {
+            if (!File.Exists(arquivoConexaoFechada))
+                File.Create(arquivoConexaoFechada).Close();
+            using (StreamWriter vWriter = new StreamWriter(arquivoConexaoFechada, true))
+            {
+                vWriter.WriteLine("\n\n******************************\n");
+                vWriter.WriteLine("Conexão fechada: " + DateTime.Now.ToString());
+                vWriter.Flush();
+                vWriter.Close();
+            }
         }
+
+        private void GetTwitter(object sender)
+        {
+            try
+            {
+                using (var getTwitter = new GetTwitter())
+                {
+                    getTwitter.GetCandidatos();
+
+                    if (!File.Exists(arquivoSuceso))
+                        File.Create(arquivoSuceso).Close();
+                    using (StreamWriter vWriter = new StreamWriter(arquivoSuceso, true))
+                    {
+                        vWriter.WriteLine("\n\n******************************\n");
+                        vWriter.WriteLine("Serviço executado com sucesso: " + DateTime.Now.ToString());
+                        vWriter.Flush();
+                        vWriter.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (!File.Exists(arquivoErro))
+                    File.Create(arquivoErro).Close();
+
+                using (StreamWriter vWriter = new StreamWriter(arquivoErro, true))
+                {
+                    vWriter.WriteLine("\n\n******************************\n");
+                    vWriter.WriteLine("Erro no serviço: " + DateTime.Now.ToString());
+                    vWriter.WriteLine("\n\nMensagem: " + ex.Message);
+                    vWriter.Flush();
+                    vWriter.Close();
+                }
+                
+                
+            }
+        }
+
     }
 }
