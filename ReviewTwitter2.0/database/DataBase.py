3bb5640e8e8e6cb7c605861a03703ae28db03167
osnjunior91 <junin096>
2018-11-22 15:42:16 -0200

    Ajuste feito para o treinamento e a avaliação dos twitters.

--- ReviewTwitter2.0/database/DataBase.py
+++ ReviewTwitter2.0/database/DataBase.py
@@ -14,7 +14,18 @@ def GetAllTwitter():
     db = client['TCC']
     twitters = db['TwitterData']
     print('Dados recuperados\n')
-    return twitters.find().limit(5);
+    return twitters.find().limit(1000);
+
+def GetAllTwitterTitulo():
+    print('Recuperando os dados\n')
+    db = client['TCC']
+    twitters = db['TwitterData']
+    print('Dados recuperados\n')
+    return twitters.find({
+        'textSearch': 'bolsonaro',
+        'textSearch': 'bolsonaro2018',
+        'textSearch': 'jairbolsonaro'
+    }).limit(10);
 
 def SaveTraining(trainings):
     try:
@@ -28,10 +39,31 @@ def SaveTraining(trainings):
     except:
         session.abort_transaction()
 
+def SaveFeeling(feelings):
+    try:
+        print('Salvando registros')
+        db = client['TCC']
+        data = db['TwitterFeeling']
+        session.start_transaction()
+        for feeling in feelings:
+            data.save(feeling)
+        session.commit_transaction()
+    except:
+        session.abort_transaction()
+
 def ExistsTwitterTraining(twitterId):
     try:
         db = client['TCC']
         training = db['Training']
+        result = training.find({'_id': twitterId}).limit(1).count()
+        return result > 0
+    except Exception as e:
+        print('Error : {0}'.format(e))
+
+def ExistsTwitterFeeling(twitterId):
+    try:
+        db = client['TCC']
+        training = db['TwitterFeeling']
         result = training.find({'_id': twitterId}).limit(1).count()
         return result > 0
     except Exception as e:
