2e9b2c7b7d7d3796960ee2236281c5d57df74a5a
osnjunior91 <junin096>
2018-11-24 20:50:44 -0200

    Ajuste feito para o treinamento e a avaliação dos twitters.

--- ReviewTwitter2.0/database/DataBase.py
+++ ReviewTwitter2.0/database/DataBase.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 import pandas as pd
+from math import exp
 import re
 
 client = MongoClient('mongodb://localhost:27017')
@@ -14,7 +16,9 @@ def GetAllTwitter():
     db = client['TCC']
     twitters = db['TwitterData']
     print('Dados recuperados\n')
-    return twitters.find().limit(1000);
+    return twitters.aggregate([{"$sample": {"size": 300}}])
+
+
 
 def GetAllTwitterTitulo():
     print('Recuperando os dados\n')
@@ -22,34 +26,73 @@ def GetAllTwitterTitulo():
     twitters = db['TwitterData']
     print('Dados recuperados\n')
     return twitters.find({
-        'textSearch': 'bolsonaro',
-        'textSearch': 'bolsonaro2018',
-        'textSearch': 'jairbolsonaro'
-    }).limit(10);
+        "$or": [
+            {'textSearch': 'bolsonaro'},
+            {'textSearch': 'bolsonaro2018'},
+            {'textSearch': 'jairbolsonaro'}
+        ]
+    }).limit(6000)
 
-def SaveTraining(trainings):
-    try:
-        print('Salvando registros')
-        db = client['TCC']
-        data = db['Training']
-        session.start_transaction()
-        for training in trainings:
-            data.save(training)
-        session.commit_transaction()
-    except:
-        session.abort_transaction()
+# def GetAllTwitterTitulo():
+#     print('Recuperando os dados\n')
+#     db = client['TCC']
+#     twitters = db['TwitterData']
+#     print('Dados recuperados\n')
+#     return twitters.find({
+#         "$or": [
+#             {'textSearch': 'haddad'},
+#             {'textSearch': 'fernandohaddad'},
+#             {'textSearch': 'haddadpresidente'}
+#         ]
+#     }).limit(6000)
+
+# def GetAllTwitterTitulo():
+#     print('Recuperando os dados\n')
+#     db = client['TCC']
+#     twitters = db['TwitterData']
+#     print('Dados recuperados\n')
+#     return twitters.find({
+#         "$or": [
+#             {'textSearch': 'cirogomes'},
+#             {'textSearch': 'ciro12'},
+#             {'textSearch': 'ciropresidente'}
+#         ]
+#     }).limit(6000);
+def SaveTraining(twitter):
+     print('Salvando registros')
+     db = client['TCC']
+     data = db['Training']
+     data.save(twitter)
 
 def SaveFeeling(feelings):
     try:
         print('Salvando registros')
         db = client['TCC']
-        data = db['TwitterFeeling']
+        data = db['TwitterFeeling2']
         session.start_transaction()
         for feeling in feelings:
             data.save(feeling)
         session.commit_transaction()
     except:
         session.abort_transaction()
+
+def SaveFeeling2(feelings):
+    try:
+        print('Salvando registros')
+        db = client['TCC']
+        data = db['Training_copy']
+        session.start_transaction()
+        for feeling in feelings:
+            data.save(feeling)
+        session.commit_transaction()
+    except:
+        session.abort_transaction()
+
+def RemoveTwitter(twitterId):
+        print('Excluindo registros')
+        db = client['TCC']
+        data = db['TwitterData']
+        data.delete_one({'_id': twitterId})
 
 def ExistsTwitterTraining(twitterId):
     try:
@@ -63,7 +106,7 @@ def ExistsTwitterTraining(twitterId):
 def ExistsTwitterFeeling(twitterId):
     try:
         db = client['TCC']
-        training = db['TwitterFeeling']
+        training = db['TwitterFeeling2']
         result = training.find({'_id': twitterId}).limit(1).count()
         return result > 0
     except Exception as e:
@@ -71,7 +114,7 @@ def ExistsTwitterFeeling(twitterId):
 
 def GetTrainingTwitter():
     db = client['TCC']
-    training = db['Training']
+    training = db['Training_copy']
     result = training.find()
     y = []
     for x in result:
